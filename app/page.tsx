630d5f36dfae852ca6c67199f0997053277b5c8e
Sifan23 <sifanyg123@gmail.com>
2025-07-27 05:43:08 +0300

    feat: imported welcome

--- app/page.tsx
+++ app/page.tsx
@@ -3,25 +3,31 @@ import NewsBlogs from "@/components/NewsBlogs";
 import ToDoList from "@/components/ToDoList";
 import { Button } from "@/components/ui/button";
 import UpcomingEvents from "@/components/UpcomingEvents";
+import Welcome from "@/components/Welcome";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
-      <div className="bg-primary-foreground p-4 rounded-lg col-span-1 lg:col-span-3">
-        <NewsBlogs />
+    <div className="w-full max-w-full">
+      <div className="w-full">
+        <Welcome />
       </div>
-      <div className="bg-primary-foreground p-4 rounded-lg col-span-1 lg:col-span-3">
-        <UpcomingEvents />
-      </div>
-      <div className="bg-primary-foreground p-4 rounded-lg">
-        <CardList title="Latest Transaction" />
-      </div>
-      <div className="bg-primary-foreground p-4 rounded-lg">
-        <ToDoList />
-      </div>
-      <div className="bg-primary-foreground p-4 rounded-lg">
-        <CardList title="Popular Content" />
+      <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
+        <div className="bg-primary-foreground p-4 rounded-lg col-span-1 lg:col-span-3">
+          <NewsBlogs />
+        </div>
+        <div className="bg-primary-foreground p-4 rounded-lg col-span-1 lg:col-span-3">
+          <UpcomingEvents />
+        </div>
+        <div className="bg-primary-foreground p-4 rounded-lg">
+          <CardList title="Latest Transaction" />
+        </div>
+        <div className="bg-primary-foreground p-4 rounded-lg">
+          <ToDoList />
+        </div>
+        <div className="bg-primary-foreground p-4 rounded-lg">
+          <CardList title="Popular Content" />
+        </div>
       </div>
     </div>
   );
