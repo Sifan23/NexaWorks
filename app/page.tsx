77174ef5146fecac58bf08c91fdd8019b0b80fb7
Sifan23 <sifanyg123@gmail.com>
2025-07-27 14:54:39 +0300

    refact: homepage code refactoring

--- app/page.tsx
+++ app/page.tsx
@@ -10,33 +10,12 @@ import EssentialResources from "@/components/sections/EssentialResources";
 
 export default function Home() {
   return (
-    <>
-      <div className="w-full max-w-full">
-        <div className="w-full pt-4 pb-4">
-          <Welcome />
-        </div>
-        <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
-          <div className="bg-primary-foreground p-4 rounded-lg col-span-1 lg:col-span-3">
-            <NewsBlogs />
-          </div>
-          <div className="bg-primary-foreground p-4 rounded-lg col-span-1 lg:col-span-3">
-            <UpcomingEvents />
-          </div>
-          {/* <div className="bg-primary-foreground p-4 rounded-lg">
-            <CardList title="Latest Transaction" />
-          </div> */}
-          {/* <div className="bg-primary-foreground p-4 rounded-lg">
-            <ToDoList />
-          </div> */}
-          {/* <div className="bg-primary-foreground p-4 rounded-lg">
-            <CardList title="Popular Content" />
-          </div> */}
-        </div>
-      </div>
-      <main>
-        <TeamSpotlight />
-        <EssentialResources />
-      </main>
-    </>
+    <main className="space-y-4 pt-4 pb-8">
+      <Welcome />
+      <NewsBlogs />
+      <UpcomingEvents />
+      <TeamSpotlight />
+      <EssentialResources />
+    </main>
   );
 }
